16438c34a31bb09b9761d34ca6f6ebf5341faaa9
Parth Patel <psyrotix@gmail.com>
2016-06-28 02:20:00 -0400

    Network requests working

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,52 +1,51 @@
 // user.service.ts
 import { Injectable } from '@angular/core';
 import {Router} from '@angular/router-deprecated';
-import { Http, Headers } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
 import {contentHeaders} from "../common/headers";
-let TEMP_URL = 'http://teeup.ddns.net:8080/cooe/profile/';
+let TEMP_URL = '//people.such.works:8080/cooe/profile/validateCredential/';
 @Injectable()
 export class UserService {
   private loggedIn = false;
-
+  headers = new Headers();
   constructor(private router: Router, private http: Http) {
     this.loggedIn = !!localStorage.getItem('auth_token');
+    this.headers.append('Content-Type', 'application/json');
   }
 
   login(username, password) {
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    let body = JSON.stringify({ username, password });
-    return this.http.post(TEMP_URL, body, { headers: contentHeaders })
+
+    return this.http.post(TEMP_URL, JSON.stringify({ "userName": username, "password": password }), { headers: this.headers })
       .subscribe(
         response => {
           localStorage.setItem('jwt', response.json().id_token);
           this.router.parent.navigateByUrl('/home');
         },
         error => {
-          alert(error.text());
-          console.log(error.text());
+          var e = JSON.parse(error.text());
+          switch(e.code){
+            //add code for invalid credentials
+            case("COOE_011"):
+              alert(e.code + '\n' + e.message);
+              break;
+            //add code for Email not validated
+            case("COOE_010"):
+              alert(e.code + '\n' + e.message);
+              break;
+            default:
+              alert(e.code);
+          }
+
+          console.log(error);
         }
       );
-    // return this.http
-    //   .post(
-    //     '/login',
-    //     JSON.stringify({ username, password }),
-    //     { headers }
-    //   )
-    //   .map(res => res.json())
-    //   .map((res) => {
-    //     if (res.success) {
-    //       localStorage.setItem('auth_token', res.auth_token);
-    //       this.loggedIn = true;
-    //     }
-    //
-    //     return res.success;
-    //   });
   }
+  validate(token){
 
+  }
   signup(username, password, firstName, lastName, email) {
     let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
+    headers.append('Content-Type', 'text/plain');
     let body = JSON.stringify({
       username,
       password,
