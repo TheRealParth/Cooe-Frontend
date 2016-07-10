2ac72430179e067cc69040b203db9475d6d82663
Parth Patel <psyrotix@gmail.com>
2016-07-09 22:29:38 -0400

    Small Changes/Comments

--- src/app/components/UI/tabbed-search.ts
+++ src/app/components/UI/tabbed-search.ts
@@ -3,4 +3,3 @@ import {Component} from '@angular/core';
 @Component({
   template: ``,
 })
-//TODO: Create the search bar for the top nav
--- src/app/components/teeup-detail.component.ts
+++ src/app/components/teeup-detail.component.ts
@@ -19,27 +19,22 @@ export class TeeupDetailComponent implements OnInit {
     id: number = -1;
     tud: TeeupDetails;
     teeupService: TeeupService;
-    localState = {
-      isMyStatusOpen: false,
-    }
     constructor(teeupService: TeeupService, private router: Router, private routeParams: RouteParams){
 
       this.teeupService = teeupService;
+      //take in the params after URL for the ID
       this.id = parseInt(routeParams.params['id']);
+      //If we have an ID, populate the teeup details
       if(this.id > -1){
         this.getTeeupDetails();
-        console.dir(this.id);
       }
+      //if no ID reroute to teeups page.
       else{
-        console.dir("else: ", this.id);
         this.router.parent.navigate(['./Teeups'])
       }
     }
 
-    toggleMyStatus(){
-      this.localState.isMyStatusOpen = !(this.localState.isMyStatusOpen);
-      console.log(this.localState.isMyStatusOpen);
-    }
+
     getTeeupDetails(){
         this.teeupService.getTeeupById(this.id).then(teeupDetails => this.tud = teeupDetails);
     }
